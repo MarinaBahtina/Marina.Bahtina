0eb73d50122c6fb13e1e9391ed32ddf1fc61b087
MarinaBahtina <marina.bahtina.16@gmail.com>
2025-04-24 18:30:42 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+'use strict'
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('Скрипт отработал корректно');
+    //Вход \\3.3 Появление модального окошка
+    const headerButtonEnter = document.querySelector(".header__enter");
+    const modalApplication = document.querySelector(".popup");
+    if (headerButtonEnter && modalApplication) {
+        headerButtonEnter.addEventListener("click", () => {
+            console.log('нажата кнопка войти');
+            modalApplication.removeAttribute("hidden");
+        });
+
+        window.addEventListener("click", (event) => {
+            if (event.target === modalApplication) {
+                modalApplication.setAttribute("hidden", true);
+            }
+        });
+
+    }
+    const closeModalButton = document.querySelector(".login__close");
+    closeModalButton.addEventListener("click", () => {
+        modalApplication.setAttribute("hidden", true);
+    });
+
+    //Регистрация
+    const headerButtonReg = document.querySelector(".header__register");
+    const modalApplicationReg = document.querySelector(".popup-reg");
+    if (headerButtonReg && headerButtonReg) {
+        headerButtonReg.addEventListener("click", () => {
+            console.log('нажата кнопка регистрация');
+            modalApplicationReg.removeAttribute("hidden");
+            
+        });
+        window.addEventListener("click", (event) => {
+            if (event.target === modalApplicationReg) {
+                modalApplicationReg.setAttribute("hidden", true);
+            }
+        });
+        const closeModalButtonReg = document.querySelector(".register__close");
+        closeModalButtonReg.addEventListener("click", () => {
+            modalApplicationReg.setAttribute("hidden", true);
+        });
+    }
+    //3.4 Фильтр
+    const buttons = document.querySelectorAll(".uslugi__btn");
+    const cardsFilter = document.querySelectorAll(".uslugi__vid");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            console.log("hello");
+            const filter = button.getAttribute("data-filter");
+            cardsFilter.forEach((card) => {
+                 if (filter === "all") {
+                    card.classList.remove("hidden");
+                    console.log("dchhuh");
+                } else {
+                    if (card.classList.contains(filter)) {
+                        card.classList.remove("hidden");
+                    } else {
+                        card.classList.add("hidden");
+                    }
+                }
+            });
+        });
+    });
+
+    
+});
